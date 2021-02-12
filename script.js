580966de8b11755ac6e99850409b7a3c838226ec
Nils Nahooy <68296142+fj0lverkr@users.noreply.github.com>
2021-02-12 16:04:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  // Create two variable with the names of the months and days in an array
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  // Run the clock
+  setInterval(function() {
+    let newDate = new Date();
+    newDate.setDate(newDate.getDate());
+    let seconds = new Date().getSeconds();
+    let minutes = new Date().getMinutes();
+    let hours = new Date().getHours();
+
+    $('#date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()]);
+
+    // Add a leading zero to  values
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    hours = (hours < 10 ? "0" : "") + hours;
+
+    $('#time').html(hours + " : " + minutes + " : " + seconds);
+
+  }, 1000);
+});
