136153f15e4c4e450d8ddeebdd910bdcb5200da0
Nils Nahooy <nils.nahooy@live.com>
2021-02-12 23:17:40 +0100

    Added searchbar for google search

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ $(document).ready(function() {
   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-  // Run the clock
+  // Run the clock once 
+  let newDate = new Date();
+  newDate.setDate(newDate.getDate());
+  let seconds = new Date().getSeconds();
+  let minutes = new Date().getMinutes();
+  let hours = new Date().getHours();
+
+  $('#date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()]);
+
+  // Add a leading zero to  values
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  hours = (hours < 10 ? "0" : "") + hours;
+
+  $('#time').html(hours + " : " + minutes + " : " + seconds);
+
+  // Now run it every second
   setInterval(function() {
     let newDate = new Date();
     newDate.setDate(newDate.getDate());
